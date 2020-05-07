11f46bf13cc6eda1186575c0eb7a65e0b207a248
Thibault Cools <thibaultcools@outlook.be>
2020-05-07 12:44:04 +0200

    Aantal programmeertalen toegevoegd en capture template om definities toe te voegen in het huidige document

--- init.el
+++ init.el
@@ -50,11 +50,13 @@
  '(emojify-emoji-styles (quote (github unicode)))
  '(inhibit-startup-screen t)
  '(lsp-prefer-flymake nil t)
+ '(lsp-ui-doc-alignment (quote window))
+ '(lsp-ui-doc-position (quote bottom))
  '(org-babel-tangle-comment-format-beg "[[%link][%source-name]]")
  '(org-link-file-path-type (quote adaptive))
  '(package-selected-packages
    (quote
-    (htmlize diminisht yasnippet-classic-snippets yasnippet-snippets yasnippet lsp-clients helm-lsp lsp-ui lsp-mode flycheck-clang-tidy flycheck rg clang-format helm-rg emojify cargo zig-mode rust-mode ob-rust rainbow-delimiters rjsx-mode pdf-tools highlight-indent-guides rainbow-mode evil company auto-complete auto-compile)))
+    (elisp-format htmlize diminisht yasnippet-classic-snippets yasnippet-snippets yasnippet lsp-clients helm-lsp lsp-ui lsp-mode flycheck-clang-tidy flycheck rg clang-format helm-rg emojify cargo zig-mode rust-mode ob-rust rainbow-delimiters rjsx-mode pdf-tools highlight-indent-guides rainbow-mode evil company auto-complete auto-compile)))
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
