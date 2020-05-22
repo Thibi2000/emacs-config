48cfd111f10a2f5063414c5176b57cf5ce0d545a
Thibault Cools <thibaultcools@outlook.be>
2020-05-22 13:37:13 +0200

    Theme zever

--- init.el
+++ init.el
@@ -47,6 +47,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("e4af99977fc5c572c8b82c19fce7afc622c6e8343bbb75230563c12dac7de4a8" default)))
  '(emojify-emoji-styles (quote (github unicode)))
  '(inhibit-startup-screen t)
  '(lsp-prefer-flymake nil t)
